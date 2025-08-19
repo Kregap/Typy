7c939a8cc2981cb8aaf2530a88e4df3f4e47dbdc
Daniel Pigan <pigan.daniel@gmail.com>
2025-08-19 21:26:54 +0200

    feat: improve icon, ensure it's used in EXE

--- scripts/svg-to-ico.ps1
+++ scripts/svg-to-ico.ps1
@@ -3,13 +3,13 @@
 # Usage: Run this script in PowerShell in the Typy folder
 
 # Convert SVG to PNG at multiple sizes
-$svg = "icon.svg"
+$svg = "src/assets/icon.svg"
 $sizes = @(16, 32, 48, 64, 128, 256)
 foreach ($size in $sizes) {
-    inkscape -o "icon-$size.png" -w $size -h $size $svg
+    inkscape.com -o "icon-$size.png" -w $size -h $size $svg
 }
 # Combine PNGs into a single ICO file
-magick convert icon-16.png icon-32.png icon-48.png icon-64.png icon-128.png icon-256.png icon.ico
+magick convert icon-16.png icon-32.png icon-48.png icon-64.png icon-128.png icon-256.png src/assets/icon.ico
 # Clean up PNGs
 Remove-Item icon-*.png
 Write-Host "icon.ico created."
